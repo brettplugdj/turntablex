236ad77517c724fd9aa0f574e9128ee9b2e6c36a
Brett Weisberg <weisbergbrett96@gmail.com>
2021-03-14 20:41:52 -0700

    Update initialize.js

--- chrome/initialize.js
+++ chrome/initialize.js
@@ -1,6 +1,6 @@
 (function(){
     var TTX = document.createElement('script');
-    TTX.setAttribute('src','https://raw.githubusercontent.com/brettplugdj/turntablex/main/js/ttx.js');
+    TTX.setAttribute('src','https://cdn.jsdelivr.net/gh/brettplugdj/turntablex@main/js/ttx.js');
     TTX.setAttribute('type','text/javascript');
     (document.body || document.head).appendChild(TTX);
 })();
